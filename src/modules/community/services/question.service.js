2cbd618ac3d19005900f5f923f2f284e4d1d28a8
Abdallah_RT <113673907+AbdallahRT@users.noreply.github.com>
2026-05-17 12:08:56 +0300

    fix: streamline answer deletion in question service by using bulk delete

--- src/modules/community/services/question.service.js
+++ src/modules/community/services/question.service.js
@@ -3,7 +3,7 @@ const Tag = require("../models/tag.model.js");
 const tagService = require("./tag.service.js");
 const { emitQuestionCreated } = require("../../../realtime/events");
 const activityEventService = require("../../gamification/services/activityEvent.service");
-const answerService = require("./answer.service.js");
+const Answer = require("../models/answer.model.js");
 const ALLOWED_CREATE_FIELDS = ["title", "body", "tags", "course", "author"];
 const ALLOWED_UPDATE_FIELDS = ["title", "body", "tags", "course"];
 
@@ -170,10 +170,7 @@ const deleteQuestion = async (id) => {
     if (question && question.tags && question.tags.length > 0) {
         await Tag.updateMany({ _id: { $in: question.tags } }, { $inc: { usageCount: -1 } });
     }
-    const answers = await answerService.getAnswersForQuestion(id);
-    for (const ans of answers.answers) {
-        await answerService.deleteAnswer(ans._id);
-    }
+    await Answer.deleteMany({ question: id });
     return await Question.findByIdAndDelete(id);
 };
 
